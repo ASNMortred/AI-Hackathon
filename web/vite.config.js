8345bdff705b3a8381496bf9fe534703ee794aae
fangxi <fangxi@fangxideMacBook-Pro-3.local>
2025-10-24 14:17:24 +0800

    feat: 增加web前端

---
+++ web/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:8080', // 后端服务地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
